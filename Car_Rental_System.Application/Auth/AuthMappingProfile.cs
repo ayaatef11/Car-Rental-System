cfc636c2f0107378a70101c2963ca8496eb6c1e8
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 16:48:35 +0300

    change authentication mapping profile

--- Car_Rental_System.Application/Auth/AuthMappingProfile.cs
+++ Car_Rental_System.Application/Auth/AuthMappingProfile.cs
@@ -1,14 +1,9 @@
-﻿
-using AutoMapper;
-using Car_Rental_System.Application.Auth.Commands.RegisterUser;
-using Car_Rental_System.Domain.Entities;
-
-namespace Car_Rental_System.Application.Auth;
+﻿namespace Car_Rental_System.Application.Auth;
 internal class AuthMappingProfile : Profile
 {
     public AuthMappingProfile()
     {
-        CreateMap<RegisterUserCommand, User>();
+        CreateMap<RegisterUserCommand, AppUser>();
 
     }
 }
