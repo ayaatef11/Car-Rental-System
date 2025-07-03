e64573e8d2e9ad9f80d6a265ad8c3eb61f474c64
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 16:48:18 +0300

    add a query to get the current user

---
+++ Car_Rental_System.Application/Auth/Queries/GetCurrentUser/GetCurrentUserQuery.cs
@@ -0,0 +1,4 @@
+﻿
+namespace Car_Rental_System.Application.Auth.Queries.GetCurrentUser;
+public record GetCurrentUserQuery(string Email) : IRequest<Result<UserDto>>;
+
---
+++ Car_Rental_System.Application/Auth/Queries/GetCurrentUser/GetCurrentUserQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Car_Rental_System.Application.Auth.Queries.GetCurrentUser;
+public class GetCurrentUserQueryHandler(UserManager<AppUser> _userManager,ILogger<GetCurrentUserQueryHandler>_logger) : IRequestHandler<GetCurrentUserQuery, Result<UserDto>>
+{
+    public async Task<Result<UserDto>> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Email))
+            {
+                return Result<UserDto>.Fail(UserErrors.NotFound(request.Email));
+            }
+
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                _logger.LogWarning($"User not found for email: {request.Email}");
+                return Result<UserDto>.Fail(UserErrors.NotFound(request.Email));
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var userDto = new UserDto
+            {
+                Id=user.Id,
+                Username = user.UserName,   
+            };
+
+            return Result<UserDto>.Ok(userDto);
+     
+    }
+}
