bdca6f74252f23c000d2db8f8b1ea30c98562a00
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 21:50:25 +0300

    add customer specifications

---
+++ Car_Rental_System.Application/Specifications/CustomerSpecifications/CustomerSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Car_Rental_System.Infrastructure.Specifications;
+
+namespace Car_Rental_System.Application.Specifications.CustomerSpecifications;
+    public class CustomerSpecification : BaseSpecification<Customer>
+    {
+        public CustomerSpecification()
+        {
+            IncludesCriteria.Add(P => P.Reservations!);
+        }
+        public CustomerSpecification(int id)
+        {
+            WhereCriteria = P => P.Id == id;
+
+            IncludesCriteria.Add(P => P.Reservations!);
+        }
+        public CustomerSpecification(CustomerSpecificationParams param)
+        {
+            WhereCriteria = P => P.Id == param.Id;
+
+            IncludesCriteria.Add(P => P.Reservations!);
+        }
+    }
---
+++ Car_Rental_System.Application/Specifications/CustomerSpecifications/CustomerSpecificationParams.cs
@@ -0,0 +1,16 @@
+﻿namespace Car_Rental_System.Application.Specifications.CustomerSpecifications;
+    public class CustomerSpecificationParams
+    {
+        public int Id { get; set; }
+        private const int MaxPageSize = 10;
+
+        private int pageSize = 5;
+        public int PageIndex { get; set; } = 1;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = value > MaxPageSize ? pageSize : value; }
+        }
+        public int? ReservationId { get; set; }
+    }
+
