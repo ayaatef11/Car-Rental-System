c42efdf9028d92021cf39b48e93bb3a00b07eef5
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 16:58:06 +0300

    add car specifications

---
+++ Car_Rental_System.Infrastructure/Specifications/CarSpecifications/Carspecification.cs
@@ -0,0 +1,26 @@
+﻿namespace Car_Rental_System.Infrastructure.Specifications.CarSpecifications;
+    internal class Carspecification : BaseSpecification<Car>
+{
+    public string? Email { get; set; }
+    public Carspecification()
+    {
+        IncludesCriteria.Add(P => P.Reservations!);
+    }
+    public Carspecification(int id)
+    {
+        WhereCriteria = P => P.Id == id;
+
+        IncludesCriteria.Add(P => P.Reservations!);
+    }
+    public Carspecification(string email)
+    {
+        Email = email;
+        //WhereCriteria = p => p.UserEmail == email;
+    }
+    public Carspecification(CarSpecificationParams param)
+    {
+        WhereCriteria = P => P.Id == param.Id;
+
+        //IncludesCriteria.Add(P => P.Order!);
+    }
+}
