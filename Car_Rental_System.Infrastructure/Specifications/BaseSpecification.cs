889535c237ac1ebbae227504dcf346debfaadcfc
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 16:58:33 +0300

    add specification evaluator and implement the interface

---
+++ Car_Rental_System.Infrastructure/Specifications/BaseSpecification.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Car_Rental_System.Infrastructure.Specifications.Interfaces;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Car_Rental_System.Infrastructure.Specifications;
+
+
+public class BaseSpecification<T> : ISpecifications<T> where T : BaseEntity
+{
+    public BaseSpecification()
+    {
+
+    }
+
+    public BaseSpecification(Expression<Func<T, bool>> whereCriteria)
+    {
+        WhereCriteria = whereCriteria;
+    }
+
+    protected virtual void AddNestedInclude(Func<IQueryable<T>, IIncludableQueryable<T, object>> nestedIncludeExpression)
+    {
+        NestedIncludes.Add(nestedIncludeExpression);
+    }
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        IncludesCriteria.Add(includeExpression);
+    }
+    public void ApplyPagination(int skip, int take)
+    {
+        IsPaginationEnabled = true;
+        Skip = skip;
+        Take = take;
+    }
+}
---
+++ Car_Rental_System.Infrastructure/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,45 @@
+﻿using Car_Rental_System.Infrastructure.Specifications.Interfaces;
+using System.Linq.Dynamic.Core;
+
+namespace Car_Rental_System.Infrastructure.Specifications;
+
+public class SpecificationsEvaluator<T> where T : BaseEntity
+{
+    public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecifications<T> spec)
+    {
+        var query = inputQuery;
+
+        if (spec.WhereCriteria != null)
+            query = query.Where(spec.WhereCriteria);
+
+        query = spec.IncludesCriteria.Aggregate(query,
+  (current, include) => current.Include(include));
+
+        foreach (var include in spec.ThenIncludes)
+        {
+            query = query.Include(include);
+        }
+
+        if (spec.OrderBy != null)
+            query = query.OrderBy(spec.OrderBy);
+
+        else if (spec.OrderByDesc != null)
+            query = query.OrderByDescending(spec.OrderByDesc);
+
+        if (spec.IsPaginationEnabled)
+            query = query.Skip(spec.Skip).Take(spec.Take);
+
+        query = spec.IncludesCriteria.Aggregate(query, (currentQuery, includeExpression) =>
+        currentQuery.Include(includeExpression));
+
+        if (spec.IncludeExpressions != null && spec.IncludeExpressions.Any())
+        {
+            query = spec.IncludeExpressions.Aggregate(query, (currentQuery, includeExpression) =>
+                includeExpression(currentQuery));
+        }
+        return query;
+    }
+
+}
+
+
