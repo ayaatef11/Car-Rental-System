f450e485fa1ce7ae11550c7d1d29070d1638df42
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 16:47:28 +0300

    use sqllite for hangfire background jobs

--- Car_Rental_System.API/GlobalUsings.cs
+++ Car_Rental_System.API/GlobalUsings.cs
@@ -2,8 +2,6 @@
 global using Car_Rental_System.Application.Cars.Commands.AddCar;
 global using Car_Rental_System.Application.Cars.Commands.DeleteCar;
 global using Car_Rental_System.Application.Cars.Commands.UpdateCar;
-global using Car_Rental_System.Application.Cars.Commands.RentCar;
-global using Car_Rental_System.Application.Cars.Commands.ReturnCar;
 global using MediatR;
 global using Car_Rental_System.Application.Cars.Queries.GetAllCars;
 global using Car_Rental_System.Application.Cars.Queries.GetAvailableCars;
@@ -29,3 +27,22 @@ global using Car_Rental_System.Application.Auth.Commands.RefreshToken;
 global using Car_Rental_System.Application.Auth.Commands.RegisterUser;
 global using Car_Rental_System.Application.Auth.Commands.ResetEmailConfirmation;
 global using Car_Rental_System.Application.Auth.Commands.ResetPassword;
+global using Car_Rental_System.API.Common;
+global using Car_Rental_System.Application.Auth.Queries.GetCurrentUser;
+global using Car_Rental_System.Domain.Entities;
+global using System.Linq.Dynamic.Core.Tokenizer;
+global using System.Security.Claims;
+global using Car_Rental_System.Application.Customers.Commands.AddCustomer;
+global using Car_Rental_System.Application.Customers.Commands.DeleteCustomer;
+global using Car_Rental_System.Application.Customers.Commands.UpdateCustomer;
+global using Car_Rental_System.Application.Customers.Queries.GetAllCustomers;
+global using Car_Rental_System.Application.Customers.Queries.GetCustomer;
+global using Car_Rental_System.Application.Customers.Queries.ViewCustomerRentalHistory;
+global using Car_Rental_System.Application.Cars.Commands.CheckCarAvailability;
+global using Car_Rental_System.Application.Reports.Queries.GetCustomerRentalReportQuery;
+
+global using Car_Rental_System.Application.Reservations.Commands.DeleteReservation;
+
+
+
+
