bf531da897220f8d1258c381f4440a0978b16283
ayaatef11 <ayabadrin667@gmail.com>
2025-07-03 16:50:34 +0300

    cet all cars query

--- Car_Rental_System.Application/Cars/Queries/GetAllCars/GetAllCarsQuery.cs
+++ Car_Rental_System.Application/Cars/Queries/GetAllCars/GetAllCarsQuery.cs
@@ -1,7 +1,5 @@
-﻿
-
-namespace Car_Rental_System.Application.Cars.Queries.GetAllCars;
-    public class GetAllCarsQuery
+﻿namespace Car_Rental_System.Application.Cars.Queries.GetAllCars;
+    public class GetAllCarsQuery:IRequest<IEnumerable<Car>>
     {
        
     }
--- Car_Rental_System.Application/Cars/Queries/GetAllCars/GetAllCarsQueryHandler.cs
+++ Car_Rental_System.Application/Cars/Queries/GetAllCars/GetAllCarsQueryHandler.cs
@@ -1,12 +1,10 @@
-﻿namespace Car_Rental_System.Application.Cars.Queries.GetAllCars
-{
-    internal class GetAllCarsQueryHandler(IUnitOfWork _unitOfWork)
+﻿namespace Car_Rental_System.Application.Cars.Queries.GetAllCars;
+    internal class GetAllCarsQueryHandler(IUnitOfWork _unitOfWork):IRequestHandler<GetAllCarsQuery,IEnumerable<Car>>
     {     
-        public async Task<IEnumerable<Car>> Handle(GetAllCarsQuery query)
+        public async Task<IEnumerable<Car>> Handle(GetAllCarsQuery query,CancellationToken c)
         {
             var cars = await _unitOfWork.Repository<Car>().GetAllAsync();
             return cars;
         }
     }
-}
 
